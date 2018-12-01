9c996b5ad61bff222fbf45aa42907f4dd5054680
Daniel Tomaszuk <daniel.tomaszuk@netguru.pl>
2018-12-01 14:02:51 +0100

    Add urls for reserve_app

--- reserve_app/urls.py
+++ reserve_app/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from reserve_app.views import ReservationDetailView
+
+app_name = "reserve_app"
 urlpatterns = [
-    path('', include('reserve_app.urls'), name='reservations'),
-    path('admin/', admin.site.urls),
+    path('', ReservationDetailView.as_view(), name='reservation_details')
 ]

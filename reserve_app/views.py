6df8075c4cb7e0287d88ac15e63fd47f69b1f151
Daniel Tomaszuk <daniel.tomaszuk@netguru.pl>
2018-12-02 14:57:11 +0100

    Add reservation view, base and reservation templates.

--- reserve_app/views.py
+++ reserve_app/views.py
@@ -1,12 +1,14 @@
-from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 
 from reserve_app.models import Reservation
 
 
-class ReservationDetailView(DetailView):
+class ReservationDetailView(ListView):
     model = Reservation
     template_name = "reserve_app/reservation_details.html"
 
     def get_queryset(self):
-        user = self.request.user.user
-        return Reservation.objects.get(user=user)
+        username = self.request.user.username
+        qs = get_object_or_404(Reservation, user__username=username)
+        return qs

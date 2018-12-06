cc8f4d658a7ef89fda0e5a83643eaa7d575e3f6c
dghy <dtomaszuk01@gmail.com>
2018-12-06 19:30:41 +0100

    Add select date widget

--- reserve_app/forms.py
+++ reserve_app/forms.py
@@ -1,7 +1,11 @@
-import datetime
 
 from django import forms
+from django.forms import SelectDateWidget
 
 
 class DateForm(forms.Form):
-    date = forms.DateField(initial=datetime.date.today)
+    date = forms.DateField(
+        widget=SelectDateWidget(
+            empty_label=("Choose Year", "Choose Month", "Choose Day"),
+        ),
+    )
